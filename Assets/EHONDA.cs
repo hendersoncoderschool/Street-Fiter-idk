23804b29d97f6dd83ac8a81c3abf415c2c31b63b
Henderson The Coder School <68570781+hendersoncoderschool@users.noreply.github.com>
2024-05-08 19:00:01 -0700

    ehonda movement

--- Assets/EHONDA.cs
+++ Assets/EHONDA.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class EHONDA : MonoBehaviour
 {
+    Animator animator;
+    public float moveSpeed;
+    public Rigidbody2D rb;
+    public float maxSpeed = 1;
+
+    public float moveUp; 
     // Start is called before the first frame update
    void Start()
     {
@@ -14,23 +20,25 @@ public class EHONDA : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown("upArrow") && isGrounded() )
+        if(Input.GetKeyDown(KeyCode.UpArrow) && isGrounded() )
         {
             animator.SetTrigger("Jump");
             //print("jump");
             rb.AddForce(Vector2.up * moveUp, ForceMode2D.Impulse);
         }
         //animator.Play();
-        float input = Input.GetAxis("Horizontal");
+        float input = Input.GetAxis("Horizontal2");
+        print(input);
         if(rb.velocity.magnitude < maxSpeed){
             rb.AddForce(Vector2.right * moveSpeed * input);
         }
 
+        /*
         if(Input.GetKeyDown("e")){
             animator.SetTrigger("punch1");
         }
 
-        if(Input.GetKey("leftArrow")|| Input.GetKey("rightArrow"))
+        if(Input.GetKey(KeyCode.LeftArrow)|| Input.GetKey(KeyCode.RightArrow))
         {
             animator.SetBool("isWalkin", true);
         }
@@ -38,6 +46,7 @@ public class EHONDA : MonoBehaviour
             animator.SetBool("isWalkin", false);
 
         }
+        */
 
     }
     private bool isGrounded()
